b07ea0b995072c961090e8c22246e16c37759742
alberto <albertobarbosa0003@gmail.com>
2022-05-05 09:10:16 -0300

    Adicionar arquivos de projeto.

---
+++ App0003/Paciente.cs
@@ -0,0 +1,37 @@
+﻿namespace App0003
+{
+    internal class Paciente
+    {
+        public string Nome { get; set; } = default!;
+        public string Sobrenome { get; set; } = default!;
+        public int Idade { get; set; }
+        public long CPF { get; set; }
+        public DateTime Chegada { get; set; } = default!;
+        public string Urgencia { get; set; } = default!;
+
+        public string PegarNome()
+        {
+            return Nome;
+        }
+        public string PegarSobrenome()
+        {
+            return Sobrenome;
+        }
+        public int PegarIdade()
+        {
+            return Idade;
+        }
+        public long PegarCPF()
+        {
+            return CPF;
+        }
+        public DateTime PegarChegada()
+        {
+            return Chegada;
+        }
+        public string PegarUrgencia()
+        {
+            return Urgencia;
+        }
+    }
+}
---
+++ App0003/PacienteRecepcao.cs
@@ -0,0 +1,23 @@
+﻿using App0003;
+
+Paciente paciente = new Paciente();
+
+paciente.Nome = Console.ReadLine();
+paciente.Sobrenome = Console.ReadLine();
+paciente.Idade = int.Parse(Console.ReadLine());
+paciente.CPF = long.Parse(Console.ReadLine());
+paciente.Urgencia = Console.ReadLine();
+paciente.Chegada = DateTime.Now;
+
+
+string[] lines = { "Nome : " + paciente.PegarNome(), "Senhor(a) : " + paciente.PegarSobrenome(), 
+    "Idade : " + paciente.PegarIdade(), "CPF : " + paciente.PegarCPF(), "Urgencia : " + paciente.PegarUrgencia(),
+     "Chegada na UPA : " + paciente.PegarChegada()};
+
+string docPath = @"C:\Users\Àlberto Barbòsa\Desktop\PastaImprime";
+
+using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+{
+    foreach (string line in lines)
+        outputFile.WriteLine(line);
+}
